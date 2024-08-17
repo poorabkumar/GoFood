5a8770b6102f837a3ef1fdfed71ea70963ed3e2e
Poorab Kumar <poorabkumarashu@gmail.com>
2024-08-18 01:45:23 +0530

    check

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success">
         <div className="container-fluid">
-          <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
+          <Link className="navbar-brand fs-1 fst-italic" to="/">HomelyBites</Link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
