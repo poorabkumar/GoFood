ccc5db582b012666c2125164954c79e5abc756c6
Poorab Kumar <poorabkumarashu@gmail.com>
2024-08-18 01:58:10 +0530

    hello

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -15,13 +15,13 @@ export default function Carousel() {
                         </form>
                     </div>
                     <div className="carousel-item active">
-                        <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTderaoZ9wdyXOZqN3GTU7WCxURswULl9_i2g&s" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="https://thecococompany.com/cdn/shop/products/Chocolate-Truffle-Pastries_grande.jpg?v=1577958105" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="https://img.etimg.com/thumb/width-1200,height-900,imgsize-567550,resizemode-75,msid-108622908/top-trending-products/kitchen-dining/outdoor/best-barbeque-grills-under-1000.jpg" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -42,18 +42,18 @@ export default function Home() {
         <div className="carousel-inner" id='carousel'>
           <div className="carousel-caption" style={{ zIndex: "10" }}>
             <div className="d-flex justify-content-center">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
               {/* <button className="btn btn-outline-success text-white bg-success" type="submit">Search</button> */}
             </div>
           </div>
           <div className="carousel-item active">
-            <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTderaoZ9wdyXOZqN3GTU7WCxURswULl9_i2g&s" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+            <img src="https://thecococompany.com/cdn/shop/products/Chocolate-Truffle-Pastries_grande.jpg?v=1577958105" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+            <img src="https://img.etimg.com/thumb/width-1200,height-900,imgsize-567550,resizemode-75,msid-108622908/top-trending-products/kitchen-dining/outdoor/best-barbeque-grills-under-1000.jpg" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
           </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
@@ -76,13 +76,13 @@ export default function Home() {
                 </div>
                 <hr />
                 {foodItem !== [] ?
-                  foodItem.filter((item) => (item.CategoryName === data.CategoryName) && (item.name.toLowerCase().includes(search.toLocaleLowerCase()))) 
+                  foodItem.filter((item) => (item.CategoryName === data.CategoryName) && (item.name.toLowerCase().includes(search.toLocaleLowerCase())))
                     .map(filterItems => {
                       return (
                         <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
-                          <Card foodItem = {filterItems}
+                          <Card foodItem={filterItems}
                             options={filterItems.options[0]}
-                            
+
                           ></Card>
                         </div>
                       )
